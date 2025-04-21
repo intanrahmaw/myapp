1112a30dbd2af31a20cdb3218c741bb81902b4b0
220102017 <220102017@mhs.udb.ac.id>
2025-04-21 05:37:34 +0000

    mengubah main, mengubah tittle

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Intan Rahmawati',
       theme: ThemeData(
         // This is the theme of your application.
         //
